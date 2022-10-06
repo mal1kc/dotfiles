89e81ddff2eda8d4de147e2532b8bbd285c1a7e0
mal1kc <59576495+mal1kc@users.noreply.github.com>
2022-10-06 19:50:52 +0300

    updated configs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,7 +3,7 @@ alias py=python
 alias create_ghub_repo="python ~/scripts/create-github-repo.py"
 alias show_sys_info="python ~/scripts/system_hardware_info.py"
 alias ls='lsd -a'
-alias ll='lsd -al'
+alias ll='lsd -lha'
 
 export FZF_DEFAULT_OPTS="--color=bg+:\#302D41,bg:\#1E1E2E,spinner:\#F8BD96,hl:\#F28FAD --color=fg:\#D9E0EE,header:\#F28FAD,info:\#DDB6F2,pointer:\#F8BD96 --color=marker:\#F8BD96,fg+:\#F2CDCD,prompt:\#DDB6F2,hl+:\#F28FAD --preview 'bat --color=always --style=numbers --line-range=:500 {}'"
 
