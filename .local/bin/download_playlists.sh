e414e4c16d81206c74eb5e260a5f66457208020f
mal1kc <59576495+mal1kc@users.noreply.github.com>
2022-12-18 13:03:02 +0300

    fixed youtube playlist download script

--- .local/bin/download_playlists.sh
+++ .local/bin/download_playlists.sh
@@ -1,24 +1,21 @@
 #!/bin/dash
 
-
-$MUSIC_DIR = ~/Music
-#yt-dlp -f "bestaudio" --continue --no-overwrites --ignore-errors -x --audio-format mp3 -o "./%(playlist_title)s/%(channel)s - %(title)s.%(ext)s" $1
+MUSIC_DIR=~/Music
 
 # define urls for playlist vars
-#
 
-$perfect = "https://music.youtube.com/playlist?list=PLUqHRQQiRBMJfU-hUWKYvJ_GBANjRezMN"
-$eyi = "https://music.youtube.com/playlist?list=PLUqHRQQiRBMLfPxNL6mTT35Ac5F0qzPC4"
-$kafa_dinle = "https://music.youtube.com/playlist?list=PLUqHRQQiRBML1XCUwJFDVRVOJ7tPhZBEI"
-$gaming_codin = "https://music.youtube.com/playlist?list=PLUqHRQQiRBMKU4CulD3QMRIyLrPPlobVO"
-$bir_pesimistin_goz_ys = "https://music.youtube.com/playlist?list=OLAK5uy_njGXl-8uKZuChOkdHVhjzDozEkv8PykBw"
+perfect="https://music.youtube.com/playlist?list=PLUqHRQQiRBMJfU-hUWKYvJ_GBANjRezMN"
+eyi="https://music.youtube.com/playlist?list=PLUqHRQQiRBMLfPxNL6mTT35Ac5F0qzPC4"
+kafa_dinle="https://music.youtube.com/playlist?list=PLUqHRQQiRBML1XCUwJFDVRVOJ7tPhZBEI"
+gaming_codin="https://music.youtube.com/playlist?list=PLUqHRQQiRBMKU4CulD3QMRIyLrPPlobVO"
+bir_pesimistin_goz_ys="https://music.youtube.com/playlist?list=OLAK5uy_njGXl-8uKZuChOkdHVhjzDozEkv8PykBw"
 
-$dnri = "https://music.youtube.com/playlist?list=PLUqHRQQiRBMKBGl9IhmtS7umsKcn3jagx"
-$anksiyete_mixtape = "https://music.youtube.com/playlist?list=PLUqHRQQiRBMIvaGgUbwu0wPoVGwRiPb8V"
-$chilly_low_vol = "https://music.youtube.com/playlist?list=PLUqHRQQiRBMIUvstb9ql5Ene4SlMo9F0O"
+dnri="https://music.youtube.com/playlist?list=PLUqHRQQiRBMKBGl9IhmtS7umsKcn3jagx"
+anksiyete_mixtape="https://music.youtube.com/playlist?list=PLUqHRQQiRBMIvaGgUbwu0wPoVGwRiPb8V"
+chilly_low_vol="https://music.youtube.com/playlist?list=PLUqHRQQiRBMIUvstb9ql5Ene4SlMo9F0O"
 
 
-function download_music_pl{
+download_music_pl(){
   if ! [ -z $2 ]
   then
     yt-dlp -f "bestaudio" --continue --no-overwrites --ignore-errors -x --audio-format mp3 -o "$MUSIC_DIR/$2/%(channel)s - %(title)s.%(ext)s" $1
@@ -33,13 +30,14 @@ then
     download_music_pl $VAR
   done
 else
-# 1st group
-download_music_pl $perfect &
-download_music_pl $eyi "eyi" &
-download_music_pl $kafa_dinle &
-download_music_pl $gaming_codin "gaming_codin"  &
-download_music_pl $bir_pesimistin_goz_ys 
-# 2nd group bigger ones
-download_music_pl $dnri "dnri" & # this playlist is very big for co work
-download_music_pl $anksiyete_mixtape "anksiyete_mixtape" &
-download_music_pl $chilly_low_vol "chilly_low_vol" & # this playlist is very big for co work
+  # 1st group
+  download_music_pl $perfect &
+  download_music_pl $eyi "eyi" &
+  download_music_pl $kafa_dinle &
+  download_music_pl $gaming_codin "gaming_codin"  &
+  download_music_pl $bir_pesimistin_goz_ys 
+  # 2nd group bigger ones
+  download_music_pl $dnri "dnri" & # this playlist is very big for co work
+  download_music_pl $anksiyete_mixtape "anksiyete_mixtape" &
+  download_music_pl $chilly_low_vol "chilly_low_vol" & # this playlist is very big for co work
+fi
