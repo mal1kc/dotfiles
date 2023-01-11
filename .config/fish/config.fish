846da8fc449a5930f274321c8c6315b8bed80999
mal1kc <59576495+mal1kc@users.noreply.github.com>
2023-01-11 15:44:04 +0300

    quality improvments

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,6 +4,14 @@ alias create_ghub_repo="python ~/scripts/create-github-repo.py"
 alias show_sys_info="python ~/scripts/system_hardware_info.py"
 alias ls='lsd -a --color=never'
 alias ll='lsd -lha --color=never'
+# prints only description for pacman search
+#alias fuzzy_pacQs="pacman -Qs | paste -d '' - - | fzf | awk '{\$1=\$2=\"\";print \$0}'| lolcat"
+# 
+#alias fuzzy_pacSs="pacman -Ss | paste -d '' - - | fzf | awk '{\$1=\$2=\"\";print \$0}'| lolcat"
+# prints all info about package
+alias fuzzy_yaySs="yay -Ss | paste -d '' - - | fzf --preview 'echo {}' --preview-window down,10% | lolcat"
+alias fuzzy_yayQs="yay -Qs | paste -d '' - - | fzf --preview 'echo {}' --preview-window down,10% | lolcat"
+
 if test "$TERM" = "xterm-kitty"
 alias ssh="kitty +kitten ssh"
 end
@@ -12,7 +20,7 @@ export EDITOR='/sbin/nvim'
 export SUDO_EDITOR=$EDITOR
 export PATH="$HOME/.local/bin:$PATH"
 
-export FZF_DEFAULT_OPTS="--black --preview 'bat --color=always --style=numbers --line-range=:500 {}'"
+export FZF_DEFAULT_OPTS="--black --preview 'bat --color=always --style=numbers --line-range=:500 {}' --height 90%"
 # export FZF_DEFAULT_OPTS="--black --color=spinner:\#F8BD96,hl:\#F28FAD --color=fg:\#D9E0EE,header:\#F28FAD,info:\#DDB6F2,pointer:\#F8BD96 --color=marker:\#F8BD96,fg+:\#F2CDCD,prompt:\#DDB6F2,hl+:\#F28FAD --preview 'bat --color=always --style=numbers --line-range=:500 {}'"
 
 export NNN_PLUG='z:autojump;f:finder;o:fzopen;p:preview-tui;b:!bat -p $nnn*;m:cmusq;d:diffs;v:imgview;'
--- .config/my_extra_configs/mount_parts.sh
+++
@@ -1,3 +0,0 @@
-udisksctl mount -b /dev/sdc2
-udisksctl mount -b /dev/nvme0n1p1
-udisksctl mount -b /dev/sdb3
