fb3cfe1a45d2b7ef9cdff32ca59e668b7c8ae775
mal1kc <59576495+mal1kc@users.noreply.github.com>
2023-03-04 17:15:39 +0300

    added move git ignore,turn script to 2 funcs

--- mgitfile.sh
+++ mgitfile.sh
@@ -3,6 +3,10 @@
 DIRECTORY=~/.git
 TARGT_DIR=~/.giti
 
+GIGNORE_F=~/.gitignore
+TARGT_GIGNORE_F=~/.notgitignore
+
+mv_gitdir() {
 if [ -d "$DIRECTORY" ]; then
  # if $DIRECTORY exists 
   mv $DIRECTORY $TARGT_DIR
@@ -13,3 +17,23 @@ else
    echo $TARGT_DIR and $DIRECTORY not found
   fi
 fi
+
+}
+
+mv_gignore()
+{
+
+if [ -f "$GIGNORE_F" ]; then
+  mv $GIGNORE_F $TARGT_GIGNORE_F
+else
+  if [ -f "$TARGT_GIGNORE_F" ]; then
+    mv $TARGT_GIGNORE_F $GIGNORE_F
+  else
+   echo $TARGT_GIGNORE_F and $GIGNORE_F not found
+  fi
+fi
+
+}
+
+mv_gitdir
+mv_gignore
