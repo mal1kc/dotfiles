7c8deca094f5bc1f64a5ce6304b58f833326ce82
mal1kc <59576495+mal1kc@users.noreply.github.com>
2023-03-13 02:15:05 +0300

    added:git-tip,updt:emacs magit ignored file stage function

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -120,3 +120,9 @@
 (add-hook 'c++-mode-hook (lambda ()
                            (lsp-deferred)
                            (platformio-conditionally-enable)))
+
+(defun magit-add-current-buffer ()
+    "Adds (with force) the file from the current buffer to the git repo"
+    (interactive)
+    (shell-command (concat "git add -f "
+               (shell-quote-argument buffer-file-name))))
