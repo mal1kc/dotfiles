a4b961e67f2b469cb3c108280b38ba5f655db105
mal1kc <59576495+mal1kc@users.noreply.github.com>
2024-12-19 18:23:05 +0300

    updt&feat: hyprland var fix & bind changes also
    - workaround: created tmux sessioned waybar starter for audio script
      don't run on sometimes after waybar crashed because of pipewire (i
      think)
    - change dotfiles_intallers pkg files and create as .ignore file manualy
      edit to normal pkg file
    
    - also edit some of the install.py for #1 but not finished

--- .local/bin/start_waybar.sh
+++ .local/bin/start_waybar.sh
@@ -1,19 +1,22 @@
 #!/bin/env sh
 # this script called by ~/.config/systemd/user/waybar.service
 
-if [ command -v waybar ] >/dev/null 2>&1; then
-	echo "waybar not found"
-	exit 1
-fi
+for cmd in waybar tmux; do
+	if [ command -v $cmd ] >/dev/null 2>&1; then
+		echo "\"$cmd\" not found"
+		exit 1
+	fi
+done
+
+tmux_session_name="waybar_start"
 
 if [ -n "$WAYLAND_DISPLAY" ]; then
 	if [ -n "$(pgrep waybar)" ]; then
 		killall -USR2 waybar
-		notify-send "Waybar restarted with SIGUSR2 :)"
+		# notify-send "Waybar restarted with SIGUSR2 :)"
 	else
-		notify-send "started waybar"
-		waybar &
-		disown
+		# notify-send "started waybar"
+		exec tmux new-session -s "start_waybar" -d waybar
 		# disown is necessary to prevent the service from blocking
 	fi
 fi
--- scripts/write_pkg_list.sh
+++ scripts/write_pkg_list.sh
@@ -1,9 +1,16 @@
 #!/bin/env sh
 
-pkg_list_file="./dotfiles_installers/pkg_list.txt"
-nix_profile_pkg_list_file="./dotfiles_installers/nix_profile_pkg_list.txt"
+pkg_list_file="./dotfiles_installers/pkg_list.txt.ignore"
+nix_profile_pkg_list_file="./dotfiles_installers/nix_profile_pkg_list.txt.ignore"
+
+printf "getting current systems package list with yay and nix profile"
 
 cd "$HOME/dotfiles" || exit 1
+
+# check if basedir exist if not exit with 1
+if ! [ -d "$(dirname "$pkg_list_file")" ] && [ -d "$(dirname "$nix_profile_pkg_list_file")" ]; then
+	exit 1
+fi
 
 truncate -s 0 "$pkg_list_file"
 # Populate package list file with package name and description
@@ -21,3 +28,5 @@ nix profile list | rg "legacyPackages.x86" | sed -n '/legacyPackages\.x86_64-lin
 if [ -d .git ]; then
 	git add "$nix_profile_pkg_list_file" "$pkg_list_file" -f
 fi
+
+printf "created pkg list files at %s %s \n" "$pkg_list_file" "$nix_profile_pkg_list_file"
