f7aa78e3ef991d8aed4230efe53445fdafd06ff6
mal1kc <59576495+mal1kc@users.noreply.github.com>
2026-07-16 16:44:31 +0300

    hyprland: added plugin hymission && update lockscreen

--- .config/hypr/plugins.lua
+++ .config/hypr/plugins.lua
@@ -5,3 +5,12 @@
   -- hyprexpo { ... }
   -- hyprbars { ... }
 ]]
+
+hl.bind("SUPER + TAB", hl.plugin.hymission.toggle)
+hl.bind("SUPER + SHIFT + A", function()
+	hl.plugin.hymission.toggle("forceall")
+end)
+hl.bind("SUPER + A", function()
+	hl.plugin.hymission.open("onlycurrentworkspace")
+end)
+hl.bind("SUPER + Escape", hl.plugin.hymission.close)
